2550d6bb5382dcc68056ae5b49568b82e230a370
Julien Midedji <Julien.Midedji@gmail.com>
2017-09-13 09:08:15 +0200

    Add font

--- src/boot.js
+++ src/boot.js
@@ -3,17 +3,18 @@ module.exports = (function(){
     const WORLD_BOUNDS = 10000;
 
     function preload() {
-        game.load.image('loadbar', 'res/loadbar.png');
+        this.load.image('loadbar', 'res/loadbar.png');
+        this.load.bitmapFont('font','res/font_0.png', 'res/font.fnt');
     }
 
     function create() {
         
        // World settings
-       game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-       game.physics.startSystem(Phaser.Physics.P2JS);
-       game.world.setBounds(0,0, WORLD_BOUNDS, WORLD_BOUNDS);
+       this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+       this.physics.startSystem(Phaser.Physics.P2JS);
+       this.world.setBounds(0,0, WORLD_BOUNDS, WORLD_BOUNDS);
 
-       this.game.state.start("load");
+       this.state.start("load");
     }
 
     return { preload: preload, create: create};
--- src/load.js
+++ src/load.js
@@ -4,20 +4,22 @@ module.exports = (function(){
 
     function preload() {
         
-        var loadbar = game.add.sprite(200, 200, 'loadbar');
-        loadbar.anchor.setTo(0.5,0.5);
-        game.load.setPreloadSprite(loadbar, 0);
-        
-        game.load.image('background', 'res/background.png');
-        game.load.image('player', 'res/player.png');
-        game.load.start();
+        var bmpText = this.add.bitmapText(10, 10, "font", "Loading");
 
-        game.load.onLoadComplete.addOnce(complete, this);
+        var loadbar = this.add.sprite(200, 200, 'loadbar');
+        loadbar.anchor.setTo(0.5,0.5);
+        this.load.setPreloadSprite(loadbar, 0);
+        
+        this.load.image('background', 'res/background.png');
+        this.load.image('player', 'res/player.png');
+        this.load.start();
+
+        this.load.onLoadComplete.addOnce(complete, this);
     }
 
     function update() {
         if (ready) {
-          game.state.start('play');
+          this.state.start('play');
         }
     }
 
