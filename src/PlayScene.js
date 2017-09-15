e61608b52252435a7da0befd8b2262f34515bd8f
Julien Midedji <Julien.Midedji@gmail.com>
2017-09-15 16:20:10 +0200

    Implement acceleration with higher thrust frequency

--- src/PlayScene.js
+++ src/PlayScene.js
@@ -1,11 +1,13 @@
 module.exports = (function(){
     
     const ROTATION_SPEED = 100; // TODO: Create own file for constants
-    const THRUST_FORCE = 50000;
+    const THRUST_FORCE = 50000; //50000
     const PLAYER_START_Y = 120;
     const PLAYER_START_X = 210;
     const SPAWN_DISTANCE = -20;
     const AUDIO_FADE_DURATION = 7300;
+    const SPEED_UP_FREQUENCY = 1;
+    const INSTABILITY_THRESHOLD = 2;
 
     var debugMode = true;
 
@@ -16,6 +18,10 @@ module.exports = (function(){
     var explosionSpawn; // TODO: Create own module for explosionspawn
     
     var mainMusic;
+
+    var intervalMargin = 0;
+    var velocityBonus = 0;
+    var thrustFrequency = 0;
 
     function create() {
 
@@ -33,16 +39,16 @@ module.exports = (function(){
 
         // Music
         var startMusic = this.add.audio('startMusic');
-        startMusic.onDecoded.add(function() {
-            startMusic.fadeIn(AUDIO_FADE_DURATION);
-        }, this);
         startMusic.onFadeComplete.add(function() {
             mainMusic = this.add.audio('mainMusic');
             mainMusic.play();
             mainMusic.loop = true;
         }, this);
+        startMusic.onDecoded.add(function() {
+            startMusic.fadeIn(AUDIO_FADE_DURATION);
+        }, this);
         var boomSound = this.add.audio('boom');
-        boomSound.volume = 0.1;
+        boomSound.volume = 0.05;
 
         // Controls
         arrowkeys = this.input.keyboard.createCursorKeys();
@@ -52,16 +58,21 @@ module.exports = (function(){
         };
     
         var thrust = function() {
+            thrustFrequency++;
+            
             boomSound.play();
+
+            this.camera.shake(0.01, 100, false);
 
             var explosion = this.add.sprite(explosionSpawn.x, explosionSpawn.y, 'explosionAtlas');
             var frames = Phaser.Animation.generateFrameNames('explosion/ex', 0, 13, '.png', 1);
             explosion.anchor.setTo(0.5);
             explosion.scale.setTo(2, 2);
             explosion.animations.add('explode', frames, 60, false, true).play();
-            
-            player.body.setZeroVelocity();
-            player.body.thrust(THRUST_FORCE);
+
+            player.body.setZeroVelocity();            
+            var acceleration = THRUST_FORCE + THRUST_FORCE * 0.1 * (Math.pow(velocityBonus, 2));
+            player.body.thrust(acceleration);
         };
 
         this.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(thrust, this);
@@ -73,6 +84,7 @@ module.exports = (function(){
     function update() { // TODO: Create a proper game loop here
 
         updateSpawn.call(this);
+        calculateAcceleration.call(this);
 
         // Keep the player moving
         player.body.thrust(100);
@@ -87,6 +99,21 @@ module.exports = (function(){
         }
     }
     
+    function calculateAcceleration() {
+        if(this.game.time.totalElapsedSeconds() > intervalMargin) {
+            intervalMargin += 1;
+            
+            if(thrustFrequency > SPEED_UP_FREQUENCY) velocityBonus++; // Increase
+            else if (velocityBonus / 2 > 1) velocityBonus /= 2; // Decrease
+            else  velocityBonus = 0; // Round down to zero
+            
+            thrustFrequency = 0;
+        }
+
+        if(velocityBonus > INSTABILITY_THRESHOLD)
+            this.camera.shake(0.002 * velocityBonus, 2000, false);
+    }
+
     function updateSpawn() {    
         var xAngle = Math.cos(player.rotation - this.math.HALF_PI);
         var yAngle = Math.sin(player.rotation - this.math.HALF_PI);
@@ -107,8 +134,11 @@ module.exports = (function(){
             this.game.debug.text('Velocity: ' + v , 32, 550);
             this.game.debug.cameraInfo(this.camera, 32, 32);
             this.game.debug.spriteCoords(player, 32, 500);
-            //this.game.debug.body(explosion);
             this.game.debug.body(player);
+
+            this.game.debug.text('velBonus: ' + velocityBonus, 32, 350);
+            this.game.debug.text('thrustFreq: ' + thrustFrequency, 32, 380);
+            this.game.debug.text('Elapsed seconds: ' + this.game.time.totalElapsedSeconds(), 32, 400);
         }
     }
 
